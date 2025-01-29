3f540e39dc1dda467e8df1bb410ecb72ad9cc710
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-01-29 13:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const par=document.querySelector('.parent')
+// console.log(par);
+// console.log(par.children[1].innerHTML);
+
+
+for(let i=0;i<par.children.length;i++){
+   console.log(par.children[i].innerHTML)
+}
+
+
+
+// par.children[1].style.color='yellow'
+// console.log(par.firstElementChild);
+// console.log(par.lastElementChild.nodeValue);
+
+
+// child to parent traversing:-
+
+const dayone=document.querySelector('.day')
+// console.log(dayone);
+// console.log(dayone.parentElement);
+// console.log(dayone.nextElementSibling);
+
+
+// console.log('nodes: ',par.childNodes);
